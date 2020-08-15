d7d830400ba9a14037ba783a875d25c2a9f6d2f7
taketoshi1214 <taketoshi1214>
2020-08-15 20:39:33 +0900

    aaa

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -3,5 +3,6 @@ class BasketsController < ApplicationController
   def show
     basket = current_user.prepare_basket
     @products = basket.products
+    @total_price = basket.total_price
   end
 end
--- app/models/basket.rb
+++ app/models/basket.rb
@@ -1,4 +1,9 @@
 class Basket < ApplicationRecord
   has_many :basket_products, dependent: :destroy
   has_many :products, through: :basket_products
+
+  def total_price
+    products.sum(:price)
+  end
+
 end
