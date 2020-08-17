3cdf95dca55c3036610f82a8d630aa2cbcfa43ac
taketoshi1214 <taketoshi1214>
2020-08-17 13:50:02 +0900

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+users= [
+{ email: 'ichiro@user.com', password: 'ichiro16' },
+]
+users.each do |record|
+ User.create!(record) unless User.find_by(email: record[:email])
+end
+
+admins= [
+{ email: 'ichiro@admin.com', password: 'ichiro16' }
+]
+admins.each do |record|
+ Admin.create!(record) unless Admin.find_by(email: record[:email])
+end
