723cd8f86941d8395b330b0f229cb8b9419a9c1e
taketoshi1214 <taketoshi1214>
2020-09-27 07:41:11 +0900

    d

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
   
-  devise_for :admins, controllers: {
-    sessions:      'admins/sessions',
-    passwords:     'admins/passwords',
-  }
-  devise_for :users, controllers: {
-    sessions:      'users/sessions',
-    passwords:     'users/passwords',
-    registrations: 'users/registrations'
-  }
+  devise_for :admins
+  devise_for :users
 
   root 'products#index'
   resource :basket
